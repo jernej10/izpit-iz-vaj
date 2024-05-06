d2a5b45866629417572ae867ecc93c8c6ff869bc
jernej10 <jernej.ofic@gmail.com>
2024-05-06 18:58:23 +0200

    Feat: data drift

---
+++ src/data/data_drift.py
@@ -0,0 +1,19 @@
+import os
+
+import pandas as pd
+from evidently.report import Report
+from evidently.metric_preset import DataDriftPreset
+
+if __name__ == "__main__":
+    report = Report(metrics=[DataDriftPreset()])
+
+    current = pd.read_csv("../../data/processed/current_data.csv")
+    reference = pd.read_csv("../../data/processed/reference_dataset.csv")
+
+    report.run(reference_data=reference, current_data=current)
+
+    if not os.path.exists("../../reports/sites"):
+        os.makedirs("../../reports/sites")
+
+    report.save_html("../../reports/sites/data_drift.html")
+    print("Data drift report created!")
